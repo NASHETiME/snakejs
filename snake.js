aa05560c083c653da6af05270804c89e0501e439
George Munyoro <32019551+georgemunyoro@users.noreply.github.com>
2020-06-20 03:49:50 +0000

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,90 +1,82 @@
 
-// George Guvamatanga
+const ctx = document.getElementById("view").getContext("2d");
 
-// Get 2d canvas context for drawing
-let canvas = document.getElementById('view').getContext('2d')
-
-/* Create the snake, which is an array of coordinates of where the
- individual pieces are */
 let snake = [
-	{ x:100, y:100 },
-	{ x:120, y:100 },
-	{ x:140, y:100 },
-	{ x:160, y:100 },
-	{ x:180, y:100 },
-]
+  { x:100, y:100 },
+  { x:120, y:100 },
+  { x:140, y:100 },
+  { x:160, y:100 },
+  { x:180, y:100 },
+];
 
-let currentSnakeDirection = 'ArrowRight'
+// Snake direction
+let snakeDirection = "ArrowRight";
 
-/* The position of the food */
-let currentFood = {x:null, y:null}
+// Position of food
+let foodLocation = { x:null, y:null };
 
-let score = 0
-
-function drawSnake(){
-	let x = false;
-	snake.forEach(piece => {
-		canvas.fillStyle = 'lime'
-		canvas.fillRect(piece.x, piece.y, 10, 10)
-		canvas.strokeRect(piece.x, piece.y, 10, 10)
-	})
+function drawSnake() {
+  snake.forEach(piece => {
+	ctx.fillStyle = "lime";
+	ctx.fillRect(piece.x, piece.y, 10, 10);
+	ctx.strokeRect(piece.x, piece.y, 10, 10);
+  })
 }
 
-function moveSnake(){
-	if(currentSnakeDirection == 'ArrowRight'){
-		snake.pop()
-		snake.unshift({x : snake[0].x + 10, y : snake[0].y})
-	}else if(currentSnakeDirection == 'ArrowLeft'){
-		snake.pop()
-		snake.unshift({x : snake[0].x - 10, y : snake[0].y})
-	}else if(currentSnakeDirection == 'ArrowDown'){
-		snake.pop()
-		snake.unshift({x : snake[0].x, y : snake[0].y + 10})
-	}else if(currentSnakeDirection == 'ArrowUp'){
-		snake.pop()
-		snake.unshift({x : snake[0].x, y : snake[0].y - 10})
-	}
+function moveSnake() {
+  snake.pop();
+  switch (snakeDirection) {
+	case "ArrowRight" : snake.unshift({ x:snake[0].x+10, y:snake[0].y });
+	case "ArrowLeft"  : snake.unshift({ x:snake[0].x-10, y:snake[0].y });
+	case "ArrowDown"  : snake.unshift({ x:snake[0].x, y:snake[0].y+10 });
+	case "ArrowUp"    : snake.unshift({ x:snake[0].x, y:snake[0].y-10 });
+  }
 }
 
-function checkForCollision(){
-	// Check if the snake has collided with the play area boundary
-	if(snake[0].x < -10 || snake[0].x > 490) return true
-	if(snake[0].y < -10 || snake[0].y > 490) return true
+function checkForCollision() {
+  // Check for collision with the play area boundary
+  if (snake[0].x < -10 || snake[0].x > 490) return true;
+  if (snake[0].y < -10 || snake[0].y > 490) return true;
 
-	// If not out of play area, check if touching food
-	let xdelta = Math.max(snake[0].x, currentFood.x) - Math.min(snake[0].x, currentFood.x)
-	let ydelta = Math.max(snake[0].y, currentFood.y) - Math.min(snake[0].y, currentFood.y)
+  // Check for collision with food
+  const xdelta = Math.abs(snake[0].x-foodLocation.x)/snake[0].x;
+  const ydelta = Math.abs(snake[0].y-foodLocation.y)/snake[0].y;
 
-	if(ydelta < 15 && xdelta < 15) generateFood(true)
+  if (ydelta < 15 && xdelta < 15) generateFood(true);
 }
 
+function generateFood(addPoint) {
+  foodLocation.x = Math.floor(Math.random()*480);
+  foodLocation.y = Math.floor(Math.random()*480);
 
-function generateFood(addPoint){
-	currentFood.x = Math.floor(Math.random()*480)
-	currentFood.y = Math.floor(Math.random()*480)
-
-	if(addPoint){
-		snake.unshift({x : snake[0].x + 10, y : snake[0].y})
-		score += 1
-		document.getElementById('score').innerHTML = score
-	}
+  if (addPoint) {
+	snake.unshift({ x:snake[0].x+10, y:snake[0].y });
+	score++;
+	document.getElementById("score").innerHTML = score;
+  }
 }
 
-function drawFood(){
-	canvas.fillStyle = 'tomato'
-	canvas.fillRect(currentFood.x, currentFood.y, 15, 15)
-	canvas.strokeRect(currentFood.x, currentFood.y, 15, 15)
+function drawFood() {
+  ctx.fillStyle = "tomato";
+  ctx.strokeRect(foodLocation.x, foodLocation.y, 15, 15);
+  ctx.fillRect(foodLocation.x, foodLocation.y, 15, 15);
 }
 
-document.addEventListener('keydown', function(e){
-	currentSnakeDirection = e.key;
-})
+document.addEventListener("keydown", event => snakeDirection = event.key);
 
-function loop(){
-	if(checkForCollision()) return
-	canvas.clearRect(0, 0, 480, 480)
-	moveSnake(); drawFood(); drawSnake()
-	requestAnimationFrame(loop, 1)
+function drawGameAssets() {
+  ctx.clearRect(0, 0, 480, 480);
+  drawSnake();
+  drawFood();
 }
 
-generateFood(); loop()
+function loop() {
+  if (checkForCollision()) return;
+  moveSnake();
+  drawGameAssets();
+  requestAnimationFrame(loop, 1);
+}
+
+generateFood();
+loop();
+
